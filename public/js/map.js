adca198bb9e21db6f917011693d35f66ceca33c5
Klas Eskilson <klas.eskilson@gmail.com>
2014-09-06 13:04:15 +0200

    Add google maps thingies

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,13 @@
 app.controller('mapsCtrl', function($scope, $http) {
+  var cities = [
+      { name: 'Linköping', longitude: 45, latitude: -73 }
+    ];
 
+  $scope.map = {
+      center: {
+          latitude: 45,
+          longitude: -73
+      },
+      zoom: 8
+  };
 });
